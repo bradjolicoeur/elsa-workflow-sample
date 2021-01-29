56ae1f1b9f252a2da040e3f22829268b5e863cc7
Brad Jolicoeur <brad@jolicoeurs.net>
2021-01-29 17:43:19 -0500

    registration page

---
+++ src/elsa-blazor/Models/RegistrationModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace elsa_blazor.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string RepeatPassword { get; set; }
+    }
+}
